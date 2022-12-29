7f59132f98f7d7dba9d8ee944910129a6f375d0c
Ruby <goatruby@gmail.com>
2022-12-29 17:24:58 +0800

    feat(Card):add adopt button

--- src/components/Card/index.vue
+++ src/components/Card/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div
-    class="flex flex-col justify-start items-start w-280px h-380px p-10px rounded-lg shadow-xl border"
+    class="flex flex-col justify-start items-start w-280px h-420px p-10px rounded-lg shadow-xl border"
   >
     <div><slot name="img"></slot></div>
     <div class="pt-10px leading-relaxed">
@@ -14,12 +14,20 @@
       <div><slot name="health-status"></slot></div>
       <div><slot name="describe"></slot></div>
     </div>
+
+    <div class="w-full flex justify-center p-10px pt-15px">
+      <n-button type="warning">我要認養</n-button>
+    </div>
   </div>
 </template>
 <script>
+import { NButton } from 'naive-ui';
 export default {
   name: 'Card',
   inheritAttrs: false,
+  components: {
+    NButton,
+  },
   props: {
     adoptInfo: { type: Object, default: [] },
   },
--- src/main.js
+++ src/main.js
@@ -4,8 +4,18 @@ import './style.css';
 import App from './App.vue';
 import router from './router/index';
 import 'virtual:windi.css';
+import {
+  // create naive ui
+  create,
+  // component
+  NButton,
+} from 'naive-ui';
+
+const naive = create({
+  components: [NButton],
+});
 
 const pinia = createPinia();
 const app = createApp(App);
-app.use(router).use(pinia).mount('#app');
+app.use(router).use(pinia).use(naive).mount('#app');
 //# sourceMappingURL=main.js.map
